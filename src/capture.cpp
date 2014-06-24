ad895db8e9689eede8ceca079aae0a78dc6b3be5
Mael Valais <mael.valais@gmail.com>
2014-06-24 10:41:06 +0200

    Ajout classe de gestion de la caméra (capture et trouver centre)

---
+++ src/capture.cpp
@@ -0,0 +1,18 @@
+/*
+ * capture.cpp
+ *
+ *  Created on: 24 juin 2014
+ *      Author: admin
+ */
+
+#include "capture.h"
+
+Capture::Capture() {
+	// TODO Stub du constructeur généré automatiquement
+
+}
+
+Capture::~Capture() {
+	// TODO !CodeTemplates.destructorstub.tododesc!
+}
+
---
+++ src/capture.h
@@ -0,0 +1,22 @@
+/*
+ * capture.h
+ *
+ *  Created on: 24 juin 2014
+ *      Author: admin
+ */
+
+#ifndef CAPTURE_H_
+#define CAPTURE_H_
+
+#include <QtCore/qobject.h>
+
+class Capture: public QObject {
+public:
+	Capture();
+	virtual ~Capture();
+
+public slots:
+
+};
+
+#endif /* CAPTURE_H_ */
