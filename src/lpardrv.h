b0e52e7a8cc92bde641c2213f0d8877858059024
mael-valais <mael.valais@gmail.com>
2014-07-04 11:52:18 +0200

    Correction de ENV_MACOS en ENV_MACOSX

--- src/lpardrv.h
+++ src/lpardrv.h
@@ -9,14 +9,6 @@ lpardrv.h - Local options for pardrv.h
 
 #define  STF_CLASS                                   /* new stuff related to the STF CCD cameras */
 
-#if defined(__APPLE__) or defined(APPLE) or defined(apple)
-#define TARGET ENV_MACOS
-#endif
-
-#if defined(LINUX) or defined(__LINUX__) or defined(linux)
-#define TARGET ENV_LINUX
-#endif
-
 #define ENV_WIN                 1                       /* Target for Windows environment */
 #define ENV_WINVXD              2                       /* SBIG Use Only, Win 9X VXD */
 #define ENV_WINSYS              3                       /* SBIG Use Only, Win NT SYS */
@@ -26,8 +18,16 @@ lpardrv.h - Local options for pardrv.h
 #define ENV_LINUX               7                       /* SBIG Use Only, Linux */
 #define ENV_NIOS            8                   /* SBIG Use Only, Embedded NIOS */
 
+#if defined(__APPLE__) or defined(APPLE) or defined(apple)
+#define TARGET ENV_MACOSX
+#endif
+
+#if defined(LINUX) or defined(__LINUX__) or defined(linux)
+#define TARGET ENV_LINUX
+#endif
+
 #ifndef TARGET
-#define TARGET         ENV_LINUX       /* Set for your default target */
+#define TARGET ENV_LINUX       /* Set for your default target */
 #endif
 
 
