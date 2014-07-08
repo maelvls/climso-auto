08e78b75b4eb3c03912691d19396d7370757c974
mael-valais <mael.valais@gmail.com>
2014-07-08 15:16:10 +0200

    Ajout de la fenetre parametres

---
+++ src/parametres.cpp
@@ -0,0 +1,20 @@
+/*
+ * parametres.cpp
+ *
+ *  Created on: 8 juil. 2014
+ *      Author: mael65
+ */
+
+#include "parametres.h"
+
+Parametres::Parametres(QWidget* parent)
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+Parametres::~Parametres()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/parametres.h
@@ -0,0 +1,20 @@
+/*
+ * parametres.h
+ *
+ *  Created on: 8 juil. 2014
+ *      Author: mael65
+ */
+
+#include <QtGui/QDialog>
+
+#ifndef PARAMETRES_H_
+#define PARAMETRES_H_
+
+class Parametres {
+public:
+	Q_OBJECT
+	Parametres(QWidget* parent);
+	virtual ~Parametres();
+};
+
+#endif /* PARAMETRES_H_ */
